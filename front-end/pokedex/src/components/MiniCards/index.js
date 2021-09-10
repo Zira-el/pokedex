2d0900a66a1b77bddb87e54981d78fc2d544f2a5
zira-el <niltonmenezes@live.com>
2021-09-10 15:36:26 -0300

    Criaçao do topo da pagina principal e css de font

---
+++ front-end/pokedex/src/components/MiniCards/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MiniCards() {
+    return(
+        <div></div>
+    )
+}
+
+export default MiniCards;
--- front-end/pokedex/src/pages/Main/index.js
+++ front-end/pokedex/src/pages/Main/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './styles.css';
+import MiniCards from '../../components/MiniCards';
+import Pokeball from '../../assets/pokebola.png'
 
 function Main() {
   return (
     <div className="App">
-      <h1>Pokedex</h1>
+
+      <div className='flex-column'>
+        <div className='flex-row content-center items-center top'>
+          <div className='pokebola-div'>
+            <img src={Pokeball} alt='pokebola' className='pokebola'/>
+          </div>
+          <div className='poppins-bold'> 
+            <h1 className='titulo' >Pokedex</h1>
+          </div>
+        </div>
+        <div className='barraPesquisa'>
+          <input placeholder='Pesquisa' type='text' className='pesquisa'/>
+        </div>
+      </div>
+     
     </div>
   );
 }
