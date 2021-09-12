56df2a6738e4bdf6db3959f5416001ef124def35
zira-el <niltonmenezes@live.com>
2021-09-11 21:01:45 -0300

    Resolvendo bug do forEach e populando os cards

--- front-end/pokedex/src/pages/Main/index.js
+++ front-end/pokedex/src/pages/Main/index.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import MiniCards from '../../components/MiniCards';
 import Pokeball from '../../assets/pokebola.png'
-import  pokedex  from '../../services/PokeAPI';
+import pokedex from '../../services/PokeAPI';
 
 function Main() {
+  const [pokemons, setPokemons] = useState([]);
 
+  async function dadosPokemons() {
+    const dados = await pokedex();
+    setPokemons(dados);
+  }
+  
   useEffect(() => {
-    pokedex()
-  })
+    dadosPokemons();
+  }, [])
 
   return (
     <div className="App">
@@ -27,7 +33,9 @@ function Main() {
         </div>
       </div>
    
-        <MiniCards />
+        <MiniCards 
+        dados={pokemons} 
+        />
      
     </div>
   );
--- front-end/pokedex/src/services/PokeAPI.js
+++ front-end/pokedex/src/services/PokeAPI.js
@@ -1,23 +1,27 @@
 
 async function pokedex() {
-    let retornoPokemon = [];
+    let retornoPokemon = []
 
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=6&offset=0`, {
-        method: 'GET'
-    });
-
-    const data = await response.json();
-
-    data.results.forEach(async link => {
-        const pokemons = await fetch(`${link.url}`, {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=6&offset=0`, {
             method: 'GET'
         });
+    
+        const {results} = await response.json();
+    
+        for (const {url} of results){
+            const pokemons = await fetch(url, {
+                method: 'GET'
+            });
+            const pokemon = await pokemons.json();
+            retornoPokemon.push(pokemon);
+        }
 
-        const pokemon = await pokemons.json();
-        retornoPokemon.push(pokemon)
-    });
+        return retornoPokemon;
+    } catch (error) {
+        return false;
+    }
 
-        console.log(retornoPokemon)
 }
 
 
